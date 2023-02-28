9f355b0f4e4fde283c422ac891379645d159a84f
aleksanchez <sfloriana8@gmail.com>
2023-02-28 13:35:52 -0800

    Updated console to display validated letters

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -6,7 +6,9 @@ const remainingGuessElement  = document.querySelector(".remaining");
 const remainingGuessSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
+
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function(word){
    const placeholderLetters = [];
@@ -21,7 +23,38 @@ placeholder(word);
 
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
+    message.innerText = "";
     const guess= letterInput.value;
-    console.log(guess);
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     letterInput.value = "";
 });
+
+
+
+
+const validateInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        message.innerText = "You need to enter a letter";
+    } else if (input.length > 1){
+        message.innerText = "You can only enter 1 letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "You can only enter a letter from A - Z";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(guess){
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)){
+        message.innerText = "You already guessed this letter.";
+        } else {
+            guessedLetters.push(guess);
+            console.log(guessedLetters);
+        }
+    };
